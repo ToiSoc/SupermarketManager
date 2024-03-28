d0482ace49b37729233631ede4720977f4733348
“GoicO” <17802018010@163.com>
2024-03-28 12:30:42 +0800

    超市管理系统

---
+++ src/main/java/com/iuexam/tools/Constants.java
@@ -0,0 +1,10 @@
+package com.iuexam.tools;
+
+public class Constants {
+	public final static String Staff_SESSION = "staffSession";
+	public final static String Staff = "staff";
+	public final static String Manager_SESSION = "managerSession";
+	public final static String SYS_MESSAGE = "message";
+	public final static int pageSize = 5;
+
+}
